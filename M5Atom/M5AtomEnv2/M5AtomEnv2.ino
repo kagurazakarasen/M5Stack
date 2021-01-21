2190723186d5c6a20f01ae877fb361a0d0add0f4
神楽坂らせん <kagurazakarasen@gmail.com>
2021-01-21 23:55:35 +0900

    雑版、プログラム構造変更
    相変わらず氷点下未テスト

--- M5Atom/M5AtomEnv2/M5AtomEnv2.ino
+++ M5Atom/M5AtomEnv2/M5AtomEnv2.ino
@@ -14,6 +14,7 @@ float pressure = 0.0;
 #define GREEN 0xf00000
 #define RED   0x00f000
 #define BLUE  0x0000f0
+#define YELLOW  0xf0f000
 #define BLACK 0x0
 
 
@@ -33,6 +34,84 @@ void setup()
     M5.dis.drawpix(0, 0xf00000);
 }
 
+void TempDisp(){  // rangeは10の位の数。０＝0～９、1＝１０～１９・・・
+
+  //tmpはグローバル、そこから計算できる
+
+  int range;
+  long cc; // color
+
+
+  if(tmp>-20){
+    range=-2;
+    cc = WHITE;
+  }
+
+  if(tmp>-10){
+    range=-1;
+    cc = WHITE;
+  }
+
+  if(tmp>0){
+    range=0;
+    cc = WHITE;
+  }
+  if(tmp>10){
+    range=1;
+    cc = BLUE;
+  }
+  if(tmp>20){
+    range=2;
+    cc = GREEN;
+  }
+  if(tmp>30){
+    range=3;
+    cc = YELLOW;
+  }
+  if(tmp>40){
+    range=4;
+    cc = RED;
+  }
+  
+  float tt = tmp - range*10;
+  float tmp2 = (int)(tt*10) / 10.0;  //小数点２位以下切り捨て
+  int tmp3 = (int)tmp2;  // 整数値
+  int tmp4 = (int)((tmp2 - (float)tmp3)*10.0);  //小数点以下（１桁）
+
+  Serial.printf("tt: %2.2f*C \r\n", tt);
+  Serial.printf("Temp2: %2.2f \r\n", tmp2);
+  Serial.printf("Temp3: %d \r\n", tmp3);
+  Serial.printf("Temp4: %d \r\n", tmp4);  
+
+
+  int i;
+
+    // tt0～10
+    for(i=0;i<11;i++){
+      if(i<tmp3) M5.dis.drawpix(i, cc);
+      else M5.dis.drawpix(i, BLACK);
+    }
+
+    //10の位
+    for(i=10;i<15;i++)  M5.dis.drawpix(i, BLACK);
+    if(range>=0){
+      M5.dis.drawpix(9+range, WHITE); 
+    }else{
+      M5.dis.drawpix(14+range, BLUE); 
+    }
+
+    
+    for(i=0;i<tmp4;i++){
+      M5.dis.drawpix(i+15, cc);
+    }
+    for(i=tmp4;i<11;i++) M5.dis.drawpix(i+15, BLACK);
+
+
+
+}
+
+
+
 void loop()
 {
 
@@ -50,20 +129,20 @@ void loop()
 
   int tmp3 = (int)tmp2;  // 整数値
 
-  Serial.printf("Temp2: %2.2f*C \r\n", tmp2);
-  Serial.printf("Temp3: %d*C \r\n", tmp3);
-
   int tmp4 = (int)((tmp2 - (float)tmp3)*10.0);  //小数点以下（１桁）
 
+/*
+  Serial.printf("Temp2: %2.2f*C \r\n", tmp2);
+  Serial.printf("Temp3: %d*C \r\n", tmp3);
   Serial.printf("Temp4: %d \r\n", tmp4);  
-
+*/
 
   int i;
 
-  for(i=10;i<15;i++)  M5.dis.drawpix(i, BLACK);
+  //for(i=10;i<15;i++)  M5.dis.drawpix(i, BLACK);
   //M5.dis.drawpix(11, 0x707070);  //White 小数点
 
-
+/*
   if(tmp3<0){ // 氷点下時
 
     if(tmp3<-10){ // マイナス10度以下
@@ -87,7 +166,34 @@ void loop()
     }
     
   } else { // プラス気温
+*/
 
+    TempDisp();
+
+/*
+    if(tmp3>0 and tmp3<11){
+
+      TempDisp(tmp,WHITE);
+    }
+
+    if(tmp3>10 and tmp3<21){
+        TempDisp(tmp-10.0,BLUE);
+    }
+
+    if(tmp3>20 and tmp3<31){
+        TempDisp(tmp-20.0,GREEN);
+    }
+
+    if(tmp3>30 and tmp3<41){
+        TempDisp(tmp-30.0,RED);
+    }
+    if(tmp3>40 and tmp3<51){
+        TempDisp(tmp-40.0,RED);
+    }
+*/
+
+
+/*    
     if(tmp3>0 and tmp3<11){
       // 0度以上10度未満
       for(i=0;i<11;i++){
@@ -101,6 +207,7 @@ void loop()
       for(i=tmp4;i<11;i++) M5.dis.drawpix(i+15, BLACK);
       //if(tmp3==0) M5.dis.drawpix(9, WHITE);
     }
+
 
     if(tmp3>10 and tmp3<21){
       // 10度以上20度未満
@@ -160,8 +267,9 @@ void loop()
       //if(tmp3==30) M5.dis.drawpix(9, RED);
     }
 
+*/
     
-  }
+//  }
 
     if (M5.Btn.wasPressed())
     {
