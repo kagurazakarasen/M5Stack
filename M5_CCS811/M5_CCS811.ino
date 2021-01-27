25860858a683c798fa57abff318f877e0b56d44d
神楽坂らせん <kagurazakarasen@gmail.com>
2021-01-27 12:04:14 +0900

    自己流修正、表示色付け
    表示を修正、シリアルプロッタ対応

--- M5_CCS811/M5_CCS811.ino
+++ M5_CCS811/M5_CCS811.ino
@@ -11,22 +11,19 @@ void setup()
 {
 
   M5.begin();
-  //M5.Power.begin();
   Wire.begin();
 
   //Serial.begin(115200);
   Serial.println();
-  //Serial.println("Apply BME280 data to CCS811 for compensation.");
   Serial.println("CCS811 data to M5Stack.");
-
 
   //This begins the CCS811 sensor and prints error status of .beginWithStatus()
   CCS811Core::CCS811_Status_e returnCode = myCCS811.beginWithStatus();
   Serial.print("CCS811 begin exited with: ");
   Serial.println(myCCS811.statusString(returnCode));
 
-    //for M5Stack 
-    M5.Lcd.setBrightness(10);
+  //for M5Stack 
+  M5.Lcd.setBrightness(10);
 }
 //---------------------------------------------------------------
 void loop()
@@ -34,18 +31,14 @@ void loop()
 
     M5.Lcd.clear();
 
-
-
   //Check to see if data is available
   if (myCCS811.dataAvailable())
   {
-
 
     //Calling this function updates the global tVOC and eCO2 variables
     myCCS811.readAlgorithmResults();
     //printInfoSerial fetches the values of tVOC and eCO2
     printInfoSerial();
-
   
   }
   else if (myCCS811.checkForStatusError())
@@ -54,7 +47,7 @@ void loop()
     printSensorError();
   }
   //for M5Stack 
-  delay(10000); //Wait for next reading
+  delay(10000); //Wait for next reading 10秒まち
 }
 
 //---------------------------------------------------------------
@@ -65,67 +58,65 @@ void printInfoSerial()
   uint16_t tvoc;
 
   //getCO2() gets the previously read data from the library
+  co2 = myCCS811.getCO2();
+
+  //getTVOC() gets the previously read data from the library
+  tvoc = myCCS811.getTVOC();
+
+  //Serial.print (Arduino シリアルプロッタ対応)
   Serial.println("CCS811 data:");
-  Serial.print(" CO2 concentration : ");
-  co2=myCCS811.getCO2();
+  Serial.print("CO2(ppm):");
   Serial.print(co2);
-  Serial.println(" ppm");
+  Serial.print(",");
+  Serial.print("TVOC(ppb):");
+  Serial.print(tvoc);
+  //Serial.println("(ppb)");
+  Serial.println("");
+
+
 
   // for M5Stack
   unsigned long sec = millis() / 1000;
 
-  if(sec >= 1200) {
+  if(sec >= 1200) { 
     M5.Lcd.setTextColor(TFT_WHITE);
   } else {
-    M5.Lcd.setTextColor(TFT_BLUE);
+    M5.Lcd.setTextColor(TFT_BLUE);  //起動後20分間は精度が出ないので青色で薄く表示
   }
   
   if(co2>999){
-    M5.Lcd.setTextColor(TFT_YELLOW);
+    M5.Lcd.setTextColor(TFT_YELLOW);  // CO2濃度が1000以上なら黄色く  
   }
   if(co2>1999){
-    M5.Lcd.setTextColor(TFT_RED);
+    M5.Lcd.setTextColor(TFT_RED);   // 2000を超えたら赤くする
   }
 
-  //M5.Lcd.plintln(co2);
-  //M5.Lcd.plintln(ppm);  
-
-
-    M5.Lcd.setCursor(10, 10);
-    M5.Lcd.setTextSize(3);
-    M5.Lcd.print("CO2 concentration");  
-    M5.Lcd.setCursor(220, 70);
-    //M5.Lcd.setTextSize(3);
-    M5.Lcd.print("ppm");  
-
+  //CO2 to LCD
+  M5.Lcd.setCursor(10, 10);
+  M5.Lcd.setTextSize(3);
+  M5.Lcd.print("CO2 concentration");  
+  M5.Lcd.setCursor(220, 70);
+  //M5.Lcd.setTextSize(3);
+  M5.Lcd.print("ppm");  
 
   M5.Lcd.setCursor(40, 50);
   M5.Lcd.setTextSize(7);
+
   M5.Lcd.print(co2);
   M5.Lcd.print(" ");
 
-
-
-  //getTVOC() gets the previously read data from the library
-  tvoc=myCCS811.getTVOC();
-  Serial.print(" TVOC concentration : ");
-  Serial.print(tvoc);
-  Serial.println(" ppb");
-
-
+  //TVOC to LCD
   M5.Lcd.setCursor(100, 150);
   M5.Lcd.setTextSize(5);
   M5.Lcd.print(tvoc);
   M5.Lcd.print(" ");
 
- 
+  M5.Lcd.setCursor(10, 165);
+  M5.Lcd.setTextSize(3);
+  M5.Lcd.print("TVOC");
 
-    M5.Lcd.setCursor(10, 165);
-    M5.Lcd.setTextSize(3);
-    M5.Lcd.print("TVOC");
-
-    M5.Lcd.setCursor(220, 165);
-    M5.Lcd.print("ppb");
+  M5.Lcd.setCursor(220, 165);
+  M5.Lcd.print("ppb");
 
 
 }
