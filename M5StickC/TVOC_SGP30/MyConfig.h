de43af463dbb70213296545d6f48c620b0128858
神楽坂らせん <kagurazakarasen@gmail.com>
2021-02-12 22:57:53 +0900

    WiFiとAmbientを使えるように変更
    自分の環境用にMyConfig.hを書き換えること

---
+++ M5StickC/TVOC_SGP30/MyConfig.h
@@ -0,0 +1,15 @@
+/*
+ * 自分の環境の設定関連を入れておくファイル
+ */
+
+// WiFi Setting
+//#define WIFI_ON   // WiFi使用時はこの行冒頭の//をカット
+const char* ssid = "zzzzz";
+const char* password = "xxxxx";
+
+// Ambient Setting
+//#define AMBIENT_ON  // Ambient使用時はこの行冒頭の//をカット
+unsigned int channelId = 123456; // AmbientのチャネルID
+const char* writeKey = "xxxxxxxxx"; // ライトキー　※書き込むほうのキー
+
+//------
--- M5StickC/TVOC_SGP30/TVOC_SGP30.ino
+++ M5StickC/TVOC_SGP30/TVOC_SGP30.ino
@@ -4,8 +4,19 @@
 
 #include <M5StickC.h>
 #include "Adafruit_SGP30.h"
+#include "Ambient.h"
+
+#include "myConfig.h" // 自分の環境の設定を入れておく
+
+
+WiFiClient client;
+Ambient ambient;
+
 
 Adafruit_SGP30 sgp;
+
+long last_millis = 0;
+unsigned long last_Anvi_millis = 0;
 
 void setup() {
     M5.begin();
@@ -13,7 +24,7 @@ void setup() {
     Wire.begin(32, 33, 100000); // ← M5StickCのI2CはSDA-G32、SLC-G33
     M5.Lcd.setRotation(1);
     M5.Lcd.fillScreen(TFT_BLACK);
-    M5.Lcd.setTextSize(2);
+    M5.Lcd.setTextSize(4);
     M5.Lcd.setTextColor(TFT_WHITE);
     M5.Lcd.setCursor(0, 0);
   
@@ -29,13 +40,43 @@ void setup() {
   Serial.print(sgp.serialnumber[1], HEX);
   Serial.println(sgp.serialnumber[2], HEX);
 
+#ifdef WIFI_ON
+  WiFi.begin(ssid, password);  //  Wi-Fi APに接続
+  while (WiFi.status() != WL_CONNECTED) {  //  Wi-Fi AP接続待ち
+      delay(100);
+  }
+
+  Serial.print("WiFi connected\r\nIP address: ");
+  Serial.println(WiFi.localIP());
+
+#endif
+
+#ifdef AMBIENT_ON
+  ambient.begin(channelId, writeKey, &client);  //  チャネルIDとライトキーを指定してAmbientの初期化
+#endif
+  
+
 }
+
+int i = 15;
 
 void loop() {
 
   unsigned long sec = millis() / 1000;
 
-  
+  while(i > 0) {    // 15秒カウントダウン
+    if(millis()- last_millis > 1000) {
+      last_millis = millis();
+      i--;
+      M5.Lcd.fillScreen(TFT_BLACK);
+      M5.Lcd.setTextSize(5);
+      //M5.Lcd.setTextColor(TFT_WHITE);
+      M5.Lcd.setCursor(0, 0);
+      M5.Lcd.print(i);      
+     }
+  }
+  M5.Lcd.setTextSize(2);
+ 
   if (! sgp.IAQmeasure()) {
     Serial.println("Measurement failed");
     return;
@@ -64,4 +105,22 @@ void loop() {
 
 
     delay(1000);
+
+#ifdef AMBIENT_ON
+  // 60秒に一回
+  if(millis()- last_Anvi_millis> 60000) {
+    last_Anvi_millis = millis();
+
+      // Ambientデータセット
+      ambient.set(1, String(sgp.eCO2).c_str());
+      ambient.set(2, String(sgp.TVOC).c_str());
+            
+      // Ambient送出
+      ambient.send();
+
+  }
+  
+#endif
+
+    
 }
